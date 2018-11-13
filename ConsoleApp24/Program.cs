3754f7c88eb64f6c3b72c0e868914d1d280e2e3c
saint12maloj <saint12maloj@gmail.com>
2018-11-13 19:33:40 +0300

    arraySort

--- ConsoleApp24/Program.cs
+++ ConsoleApp24/Program.cs
@@ -12,42 +12,32 @@ namespace ConsoleApp24
         {
             {
 
-                Console.WriteLine("Создадим массив!" + "\n" + "Введите количество элементов массива!");
-                int n = Convert.ToInt32(Console.ReadLine());
-                int[] array = new int[n];
-                Random value = new Random();
-                Console.WriteLine("Значения элементам массива присвоены случайным образом в диапазоне от -100 до 100.");
-                Console.WriteLine("Значения элементов массива следующие:");
+                int[] array = new int[] { 5, 12, 3, 44, 1, 6 };
                 for (int i = 0; i < array.Length; i++)
                 {
-                    array[i] = value.Next(-100, 100);
-                    Console.WriteLine(array[i]);
+                    Console.WriteLine("Элемент неотсортированного массива [" + i + "]:" + array[i] + ";");
                 }
-                Console.WriteLine("Найдем минимальное значение элементов получившегося массива.");
-                int min = int.MaxValue;
+                Console.WriteLine("\n");
 
                 for (int i = 0; i < array.Length; i++)
                 {
-                    if (min > array[i])
+                    for (int j = i + 1; j < array.Length; j++)
                     {
-                        min = array[i];
+                        if (array[i] > array[j])
+                        {
+                            int t = array[i];
+                            array[i] = array[j];
+                            array[j] = t;
+                        }
                     }
+
+                    Console.WriteLine("Элемент отсортированного массива [" + i + "]:" + array[i] + ";");
                 }
-                Console.WriteLine("\nМинимальное значение элементов получившегося массива-" + " " + min + ";");
-                int max = int.MinValue;
-                for (int i = 0; i < array.Length; i++)
-                {
-                    if (max < array[i])
-                    {
-                        max = array[i];
-                    }
-                }
-                Console.WriteLine("\nМаксимальное значение элементов получившегося массива-" + " " + max + ";");
-                Console.Beep();
+
+                Console.ReadLine();
+
             }
-            Console.ReadLine();
-
         }
     }
 }
- 
+    
