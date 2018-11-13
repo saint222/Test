65c3a56dbc67b6ddf5eb70e856acb11807fc9a60
saint12maloj <saint12maloj@gmail.com>
2018-11-13 19:06:16 +0300

    Add project files.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                
+                List<string> names = new List<string> {"Bob", "555", "Alex"};
+                names.Sort();
+                for (int i = 0; i < names.Count; i++)
+                {
+                    Console.WriteLine(names [i]);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
