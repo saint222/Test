7dfcf91e802b8371a264b2fa3611554ae3185f46
saint12maloj <saint12maloj@gmail.com>
2018-11-13 19:27:37 +0300

    HomeWorkTask2

--- ConsoleApp24/Program.cs
+++ ConsoleApp24/Program.cs
@@ -11,17 +11,43 @@ namespace ConsoleApp24
         static void Main(string[] args)
         {
             {
-                
-                List<string> names = new List<string> {"Bob", "555", "Alex"};
-                names.Sort();
-                for (int i = 0; i < names.Count; i++)
+
+                Console.WriteLine("Создадим массив!" + "\n" + "Введите количество элементов массива!");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] array = new int[n];
+                Random value = new Random();
+                Console.WriteLine("Значения элементам массива присвоены случайным образом в диапазоне от -100 до 100.");
+                Console.WriteLine("Значения элементов массива следующие:");
+                for (int i = 0; i < array.Length; i++)
                 {
-                    Console.WriteLine(names [i]);
+                    array[i] = value.Next(-100, 100);
+                    Console.WriteLine(array[i]);
                 }
-                Console.WriteLine("Hello Git");
-                //learning to commit
-                Console.ReadLine();
+                Console.WriteLine("Найдем минимальное значение элементов получившегося массива.");
+                int min = int.MaxValue;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (min > array[i])
+                    {
+                        min = array[i];
+                    }
+                }
+                Console.WriteLine("\nМинимальное значение элементов получившегося массива-" + " " + min + ";");
+                int max = int.MinValue;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (max < array[i])
+                    {
+                        max = array[i];
+                    }
+                }
+                Console.WriteLine("\nМаксимальное значение элементов получившегося массива-" + " " + max + ";");
+                Console.Beep();
             }
+            Console.ReadLine();
+
         }
     }
 }
+ 
